c6305b6e6a92dd56ab8a57f6565e87cb82dcea8f
Jiachen lu <37426663+JiachenLu@users.noreply.github.com>
2018-11-07 13:58:04 -0800

    Add files via upload

--- NEW_ASTEX/RESTART.sh
+++ NEW_ASTEX/RESTART.sh
@@ -1,13 +1,14 @@
 
-CYCLE=32
-
+CYCLE=8
+hr='08'
+expnr='002'
 for i in `seq $CYCLE`; do
 
 temp=$((i-1))
 	if [ "$temp" -lt "10" ]; then
-ln -s inits12h00mx000y00$temp.001 inits12h00mx000y00$temp
+ln -s inits${hr}h00mx000y00$temp.${expnr} inits${hr}h00mx000y00$temp
 else
-ln -s inits12h00mx000y0$temp.001 inits12h00mx000y0$temp
+ln -s inits${hr}h00mx000y0$temp.${expnr} inits${hr}h00mx000y0$temp
 fi
 
 
@@ -18,13 +19,13 @@ for i in `seq $CYCLE`; do
 
 temp=$((i-1))
 if [ "$temp" -lt "10" ]; then
-ln -s initd12h00mx000y00$temp.001 initd12h00mx000y00$temp
+ln -s initd${hr}h00mx000y00$temp.${expnr} initd${hr}h00mx000y00$temp
 else
-ln -s initd12h00mx000y0$temp.001 initd12h00mx000y0$temp
+ln -s initd${hr}h00mx000y0$temp.${expnr} initd${hr}h00mx000y0$temp
 fi
 
 
 echo 'linking' $i 'restart file'
 done
 
-echo 'Finished linking'
+echo 'linking finished'

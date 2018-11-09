0c1be7b6ee2e91864a0a8ba4ae752e73f0b51136
Jiachen lu <37426663+JiachenLu@users.noreply.github.com>
2018-11-09 13:13:20 -0800

    Update RESTART.sh

--- NEW_ASTEX/RESTART.sh
+++ NEW_ASTEX/RESTART.sh
@@ -3,29 +3,9 @@ CYCLE=8
 hr='08' # add '0' if the number is less than 10
 expnr='002'
 for i in `seq $CYCLE`; do
-
 temp=$((i-1))
-	if [ "$temp" -lt "10" ]; then
 ln -s inits${hr}h00mx000y00$temp.${expnr} inits${hr}h00mx000y00$temp
-else
-ln -s inits${hr}h00mx000y0$temp.${expnr} inits${hr}h00mx000y0$temp
-fi
-
-
-echo 'linking' $i 'restart file'
-done
-
-for i in `seq $CYCLE`; do
-
-temp=$((i-1))
-if [ "$temp" -lt "10" ]; then
 ln -s initd${hr}h00mx000y00$temp.${expnr} initd${hr}h00mx000y00$temp
-else
-ln -s initd${hr}h00mx000y0$temp.${expnr} initd${hr}h00mx000y0$temp
-fi
-
-
 echo 'linking' $i 'restart file'
 done
-
 echo 'linking finished'

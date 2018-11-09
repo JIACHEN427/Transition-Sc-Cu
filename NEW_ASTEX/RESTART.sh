269b2a98dcbba7c76a440c54480e3338196c375d
Jiachen lu <37426663+JiachenLu@users.noreply.github.com>
2018-11-09 13:33:48 -0800

    Update RESTART.sh

--- NEW_ASTEX/RESTART.sh
+++ NEW_ASTEX/RESTART.sh
@@ -1,12 +1,13 @@
-# Used restart the DALES from designated time.
+# A stupid shell script used restart the DALES from designated time.
 # for a in `find . -type l`; do rm $a ;done # To remove all the link in the working dir
-CYCLE=8
-hr='08' # add '0' if the number is less than 10
+CYCLE=32 # Total cores used to run the simulation
+hr='04' # add '0' if the number is less than 10
 expnr='002'
 for i in `seq $CYCLE`; do
 temp=$((i-1))
-ln -s inits${hr}h00mx000y00$temp.${expnr} inits${hr}h00mx000y00$temp
-ln -s initd${hr}h00mx000y00$temp.${expnr} initd${hr}h00mx000y00$temp
+temp=$(printf "%03d" "$temp")
+ln -s inits${hr}h00mx000y$temp.${expnr} inits${hr}h00mx000y$temp
+ln -s initd${hr}h00mx000y$temp.${expnr} initd${hr}h00mx000y$temp
 echo 'linking' $i 'restart file'
 done
 echo 'linking finished'

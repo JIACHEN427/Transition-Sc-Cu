ce1902fd6cbd56fbb42562c711a97ee213402e40
Jiachen lu <37426663+JiachenLu@users.noreply.github.com>
2018-12-02 21:27:11 -0800

    Update make427.m

--- NEW_ASTEX/make427.m
+++ NEW_ASTEX/make427.m
@@ -153,3 +153,35 @@ fprintf(fid,'%4.1f %2.0f %2.0f %1.6f %1.6f %3.6f %1.6f %1.6f\n',Var(i,:));
 %fprintf(fid,'\r\n');
 end
 fclose(fid);
+%% Matlab code used to create initial profile of the two passive scalars introduced during ASTEX in DALES. Ref. DAVINI(2017)
+%% sc.inp
+%%  End of configure
+zi=660; % Initial inversion height
+sc1=zeros(1,length(z));
+sc2=zeros(1,length(z));
+for i=1:size(z,1)
+    sc1(i)=z(i)/z(end)*1.5;
+    if z(i)>zi
+        sc2(i)=1;
+    end
+end
+head1=(['#ASTEX case using dz = ' '5' 'm, Nlev ='  '427']);
+head2=('#height sc1 sc2');
+Var=[z';sc1;sc2];
+fid=fopen(['sc.inp.' expnr],'w');
+fprintf(fid,head1);
+fprintf(fid,'\r\n');
+fprintf(fid,head2);
+fprintf(fid,'\r\n');
+for i=1:length(z)
+fprintf(fid,'%4.1f %1.1f %1.1f\n',Var(:,i));
+%fprintf(fid,'\r\n');
+end
+fclose(fid);
+% %% check the passive scalar concentration
+% plot(sc1,z)
+% plot(sc2,z)
+% %% Do the plot as in DAVINI's 
+% plot(diff(z),z(1:end-1))
+% xlabel('\Delta z [m]')
+% ylabel('Height [m]')
